da6bccbeea219cfde887cadc39545cf89fc6507e
Alexandre Marques <alexandre.morais.marques@gmail.com>
2017-12-06 15:08:49 +0000

    Junit
    Junit

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,7 @@
-import org.junit.Test;
-import static org.junit.Assert.assertEquals;
 import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
 
-class TestJunit {
-   @Test
-   public void testAdd() {
-      String str= "abc";
-      assertEquals("abc",str);
-   }
-}
 
 public class Main {
    public static void main(String[] args) {
