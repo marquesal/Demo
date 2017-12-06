648e64da5bdf62670f1ea8ba2765fd890b5bab7f
Alexandre Marques <alexandre.morais.marques@gmail.com>
2017-12-06 14:41:26 +0000

    Projecto Demo
    Projecto Demo

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+class TestJunit {
+   @Test
+   public void testAdd() {
+      String str= "abc";
+      assertEquals("abc",str);
+   }
+}
+
+public class Main {
+   public static void main(String[] args) {
+      Result result = JUnitCore.runClasses(TestJunit.class);
+      for (Failure failure : result.getFailures()) {
+         System.out.println(failure.toString());
+      }
+      System.out.println(result.wasSuccessful());
+   }
+}
